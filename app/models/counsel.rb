a3d9d4a3bb148eebd1b31ac6713b3d13f0bfe636
Yongsu Baek <dydtnrkwkd@kaist.ac.kr>
2015-09-12 08:02:04 +0000

    hos_reply destroy

--- app/models/counsel.rb
+++ app/models/counsel.rb
@@ -1,8 +1,10 @@
 class Counsel < ActiveRecord::Base
     
+    belongs_to :user
     belongs_to :codi
     # 상담 기능 구현 후 활성화
     # has_one :counsel_content
     has_one :chat_room
     belongs_to :requesting
+    
 end

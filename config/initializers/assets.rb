013fb0394700d75af7f0494cb4368e4e0f85adde
kuksugen <kuksugen@kaist.ac.kr>
2015-09-13 12:13:03 +0000

    please

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,5 +15,4 @@ Rails.application.config.assets.precompile += %w( base/login3.css )
 
 Rails.application.config.assets.precompile += %w( base/jquery.min.js base/jquery-migrate.min.js base/jquery-ui.min.js base/bootstrap.min.js base/bootstrap-hover-dropdown.min.js base/jquery.slimscroll.min.js base/jquery.blockui.min.js base/jquery.cokie.min.js base/jquery.uniform.min.js base/bootstrap-switch.min.js base/jquery.vmap.js base/jquery.vmap.russia.js base/jquery.vmap.world.js base/jquery.vmap.europe.js base/jquery.vmap.germany.js base/jquery.vmap.usa.js base/jquery.vmap.sampledata.js base/jquery.flot.min.js base/jquery.flot.resize.min.js base/jquery.flot.categories.min.js base/jquery.pulsate.min.js base/moment.min.js base/daterangepicker.js base/morris.min.js base/raphael-min.js base/jquery.sparkline.min.js base/metronic.js base/layout.js base/quick-sidebar.js base/demo.js base/index3.js base/tasks.js base/login.js base/select2.min.js base/jquery.validate.min.js )
 
-
  

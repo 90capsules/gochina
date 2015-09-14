076f10559e7bf3194a14a9f9bc20190d174b2aff
Yongsu Baek <dydtnrkwkd@kaist.ac.kr>
2015-09-14 20:57:43 +0000

    fix estimating/requesting

--- app/controllers/estimating_controller.rb
+++ app/controllers/estimating_controller.rb
@@ -1,10 +1,21 @@
 class EstimatingController < ApplicationController
   def requesting
+    @tags = {
+      'eye' => ['眼睛', ['눈 질문1','눈 질문2','눈 질문3','눈 질문4','눈 질문5']],
+      'nose'=>['鼻子', ['코 질문1','코 질문2','코 질문3','코 질문4','코 질문5']],
+      'breast'=>['乳房', ['가슴 질문1','가슴 질문2','가슴 질문3','가슴 질문4','가슴 질문5']],
+      'fat'=>['吸脂', ['지방 질문1','지방 질문2','지방 질문3','지방 질문4','지방 질문5']]
+      }
     
-    current_user.profile_img
     
+
     
+  
     
+
+  end
+
+  def requesting_complete
     req = Requesting.new
     req.priority = params[:hh]
     
@@ -22,12 +33,7 @@ class EstimatingController < ApplicationController
     req.drug_sideeffect = params[:hh]
     req.switch = params[:hh]
     
-  
-    
-
-  end
-
-  def requesting_complete
+    render :text => ""
   end
 
   def manage
